ee25c1b9a61be329b8070f8afa76e65d400ec241
David Quintanel <david.quintanel@kisio.com>
2020-03-23 16:53:29 +0100

    changed error doc

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,7 @@
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-/// The specific type of an error.
+/// Typed error for `collections`.
 pub enum Error {
     /// This error occurs when an identifier already exists.
     #[error("identifier {0} already exists")]
