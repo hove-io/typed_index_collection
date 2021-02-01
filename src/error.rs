219b78b0299461783b821277acd919f77e9e6fb6
Tristram Gräbener <tristram@midnighttrains.com>
2021-02-01 09:35:58 +0100

    Add use crate::collection::Id

--- src/error.rs
+++ src/error.rs
@@ -1,14 +1,15 @@
+use crate::collection::Id;
 use thiserror::Error;
 
 #[derive(Error)]
 /// Typed error for `typed_index_collection`.
-pub enum Error<T: crate::collection::Id<T>> {
+pub enum Error<T: Id<T>> {
     /// This error occurs when an identifier already exists.
     #[error("identifier {} already exists", .0.id())]
     IdentifierAlreadyExists(T),
 }
 
-impl<T: crate::collection::Id<T>> std::fmt::Debug for Error<T> {
+impl<T: Id<T>> std::fmt::Debug for Error<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::IdentifierAlreadyExists(obj) => f
