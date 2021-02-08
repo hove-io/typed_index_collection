30fe86064a2b818ea644af42443cef9e38c911d7
Tristram Gräbener <tristram@midnighttrains.com>
2021-02-08 11:49:37 +0100

    Doctest for Error IdentifierAlreadyExists

--- src/error.rs
+++ src/error.rs
@@ -3,6 +3,21 @@ use thiserror::Error;
 
 #[derive(Error)]
 /// Typed error for `typed_index_collection`.
+/// ```
+/// # use typed_index_collection::{Error, Id};
+/// struct Object;
+/// impl Id<Object> for Object {
+///   fn id(&self) -> &str { "object_id" }
+///   fn set_id(&mut self, _: String) { todo!() }
+/// }
+/// let error = Error::IdentifierAlreadyExists(Object);
+/// let msg = format!("{:?}", error);
+/// // Output:
+/// // IdentifierAlreadyExists Error { id: "object_id", type: "Object" }
+/// # assert!(msg.contains("IdentifierAlreadyExists"));
+/// # assert!(msg.contains("object_id"));
+/// # assert!(msg.contains("Object"));
+/// ```
 pub enum Error<T: Id<T>> {
     /// This error occurs when an identifier already exists.
     #[error("identifier {} already exists", .0.id())]
