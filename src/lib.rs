eb27a45fad742d008e373eade0d0ffbbb6ee1bf0
Jean SIMARD <woshilapin@tuziwo.info>
2023-09-11 16:27:41 +0200

    fix: clippy is not happy with 'Clone' implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 //! Collections of objects with typed indices and buildin identifier support.
 
 #![deny(missing_docs)]
+// `derivative` does not implement `Clone` the correct way if it also implements `Copy`
+#![allow(clippy::incorrect_clone_impl_on_copy_type)]
 
 mod collection;
 mod error;
